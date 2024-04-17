be96f912e8400fe4020479f94eecc7139c877d85
Rishav Kumar <rishav.kumar9@outlook.com>
2024-04-17 15:40:49 +0100

    Created Multiply Program

---
+++ multiply.py
@@ -0,0 +1,12 @@
+import os
+os.system('cls')
+os.system('whoami')
+os.system('hostname')
+os.system('date /t')
+os.system('time /t')
+print("This program multiplies two numbers.")
+print("\n")
+firstnum = int(input("Enter first number: "))
+secondnum = int(input("Enter second number: "))
+product = firstnum * secondnum
+print("Product:", product)
