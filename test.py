d9c81a08b137381d93beed7154254cc324aa7ae0
Rishav Kumar <rishav.kumar9@outlook.com>
2024-04-24 07:48:28 +0100

    created test.py as array manipulation

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
 os.system('cls')
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-print("The product is", num1 * num2)
+num1 = input("Enter any two digits number: ")
+product = int(num1[0]) * int(num1[1])
+print("The product is", product)
