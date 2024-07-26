160db8a81d0a6af0d565f04c167d16955d2856f6
Rishav Kumar <rishav.kumar9@outlook.com>
2024-07-26 09:57:11 +0100

    variables

--- multiply.py
+++ multiply.py
@@ -1,12 +1,15 @@
 import os
+import sys
 os.system('cls')
 os.system('whoami')
 os.system('hostname')
 os.system('date /t')
 os.system('time /t')
+print("Python Version: " + sys.version)
 print("This program multiplies two numbers.")
 print("\n")
 firstnum = int(input("Enter first number: "))
 secondnum = int(input("Enter second number: "))
 product = firstnum * secondnum
-print("Product:", product)
+print("Product of:" + str(firstnum) + " and " + str(secondnum) + " is " + product)
+   
---
+++ variables.py
@@ -0,0 +1,13 @@
+a,b,c = "rishav","abhijith","shubham"
+print(a)
+print(b)
+print(c)
+d=e=f="Sree"
+print(d)
+print(e)
+print(f)
+Team = ["prabhat","saurabh","raj"]
+g,h,i = Team
+print(g)
+print(h)
+print(i)
